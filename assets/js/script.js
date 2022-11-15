180a90a7ef5ca8b96df8415f83b79b6872d2d252
laps22 <lianaurora@gmail.com>
2022-11-15 18:47:50 -0500

    Updated JS with API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,31 @@
-var activity = 'skiing'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + activity,
-    headers: { 'X-Api-Key': 'FdXrMVff6H8zgtnJAdfq2A==B6BXyui3gnWe3vno'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  var dropdowns = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(dropdowns, '');
 });
-  
-  
-  
-  
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var dropdowns = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(dropdowns, '');
-  });
 
-//  // var getActivity = function (activity){
-//     var apiUrl = 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + activtiy;
-  
-//     fetch(apiUrl)
-//     .then(function (response) {
-//         if (response.ok) {
-//             response.json().then(function (data){
-//                 displayActivity(activity);
-//             });
-//         }
-//     })
-//     .catch(function (error) {
-//         alert('Unable to connect to API');
-//     });
-//   };
+// add event listener to onclick
+
+var getActivity = function (activity) {
+  var apiUrl = 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + activity;
+
+  fetch(apiUrl, {
+    headers: { 'X-Api-Key': 'FdXrMVff6H8zgtnJAdfq2A==B6BXyui3gnWe3vno' },
+    contentType: 'application/json',
+  })
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayActivity(activity);
+        });
+      }
+    })
+    .catch(function (error) {
+      // alert('Unable to connect to API');
+      console.log(error)
+    });
+}
+
+getActivity('yoga');
+
+
+
