4a9f25d4f6922ad1535ed0d16e74c3125032202a
laps22 <lianaurora@gmail.com>
2022-11-16 22:24:03 -0500

    adding cards for playlist diplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,10 @@ var checkboxes = document.querySelectorAll('input[name="Genre"]');
 var selectedGenres = document.querySelectorAll('input[name="Genre"]:checked');
 
 //If statement to display error message If no values are picked
-checkboxes.forEach(function(checkbox) {
-    if (checkbox.checked==false) 
-    alert('Please select your Genre');
-});
+// checkboxes.forEach(function(checkbox) {
+//     if (checkbox.checked==false) 
+//     alert('Please select your Genre');
+// });
 
 //set values that will go into final API URL
 var genreID = selectedGenres.value;
