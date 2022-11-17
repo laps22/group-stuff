86d37355819a3bd85f3a5ae0557c3f993e8e67ab
Kenji Fleming <kwf1999@gmail.com>
2022-11-16 21:06:45 -0500

    merged site toggle branch to main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-var darkToggle = document.querySelector('#darkToggle');
-
-// dark mode / hopefully explicit filter later on
-darkToggle.addEventListener('click', ()=> {
-  document.body.classList.toggle('dark');
-})
-
-// Slider for quantity of songs requested
-var sliderValue = document.getElementById("myRange");
-var output = document.getElementById("slideOutput");
-output.innerHTML = sliderValue.value;
-
-sliderValue.oninput = function() {
-  output.innerHTML = this.value;
-}
-
-const searchElem = document.querySelector('#search');
-searchElem.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        getContent(searchElem.value)
-    }
-});
-
-const getContent = (search) => {
-    const url = new URL('https://itunes.apple.com/search');
-    var rangeValue = sliderValue.value;
-    const params = { term: search, media: 'music', limit: rangeValue  };
-    url.search = new URLSearchParams(params);
-    fetch(url, { method: 'POST' })
-        .then(results => results.json())
-};
-
-=======
 var selectOptions = document.getElementById('activity')
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -72,5 +38,35 @@ var displayActivity = function (activity, value) {
 
 //Event listener once value has been selected
 selectOptions.addEventListener('change', formSelect)
->>>>>>> 62c6142983581e4b12d21ef70bdf4aaa3988ac4a
 
+var darkToggle = document.querySelector('#darkToggle');
+
+// dark mode / hopefully explicit filter later on
+darkToggle.addEventListener('click', ()=> {
+  document.body.classList.toggle('dark');
+})
+
+// Slider for quantity of songs requested
+var sliderValue = document.getElementById("myRange");
+var output = document.getElementById("slideOutput");
+output.innerHTML = sliderValue.value;
+
+sliderValue.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+const searchElem = document.querySelector('#search');
+searchElem.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        getContent(searchElem.value)
+    }
+});
+
+const getContent = (search) => {
+    const url = new URL('https://itunes.apple.com/search');
+    var rangeValue = sliderValue.value;
+    const params = { term: search, media: 'music', limit: rangeValue  };
+    url.search = new URLSearchParams(params);
+    fetch(url, { method: 'POST' })
+        .then(results => results.json())
+};
