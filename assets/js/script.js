6910bd935d2a50795888e18141fdc8f91d292c9f
laps22 <lianaurora@gmail.com>
2022-11-16 21:41:19 -0500

    edited HTML and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ selectOptions.addEventListener('change', formSelect)
 var darkToggle = document.querySelector('#darkToggle');
 
 // dark mode / hopefully explicit filter later on
-darkToggle.addEventListener('click', ()=> {
+darkToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 })
 
@@ -51,22 +51,22 @@ var sliderValue = document.getElementById("myRange");
 var output = document.getElementById("slideOutput");
 output.innerHTML = sliderValue.value;
 
-sliderValue.oninput = function() {
+sliderValue.oninput = function () {
   output.innerHTML = this.value;
 }
 
 const searchElem = document.querySelector('#search');
 searchElem.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        getContent(searchElem.value)
-    }
+  if (event.key === 'Enter') {
+    getContent(searchElem.value)
+  }
 });
 
 const getContent = (search) => {
-    const url = new URL('https://itunes.apple.com/search');
-    var rangeValue = sliderValue.value;
-    const params = { term: search, media: 'music', limit: rangeValue  };
-    url.search = new URLSearchParams(params);
-    fetch(url, { method: 'POST' })
-        .then(results => results.json())
+  const url = new URL('https://itunes.apple.com/search');
+  var rangeValue = sliderValue.value;
+  const params = { term: search, media: 'music', limit: rangeValue };
+  url.search = new URLSearchParams(params);
+  fetch(url, { method: 'POST' })
+    .then(results => results.json())
 };
